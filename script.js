0de5a43d651b48b68a12c07ab4919e69c319f49a
samiduhjoseph <95076471+samiduhjoseph@users.noreply.github.com>
2024-02-27 13:35:11 +0100

    accordion changes

--- script.js
+++ script.js
@@ -14,20 +14,64 @@ let icon = document.querySelectorAll(".accordion--iconplus");
 //     }
 //   });
 // }
-accordionHead.forEach((head) => {
-  head.addEventListener("click", () => {
-    head.classList.toggle("active");
+// accordionHead.forEach((head) => {
+//   head.addEventListener("click", () => {
+//     head.classList.toggle("active");
 
-    let accordionBody = head.nextElementSibling;
-    let image = head.firstElementChild;
-    // console.log(image);
+//     let accordionBody = head.nextElementSibling;
+//     let image = head.firstElementChild;
+//     // console.log(image);
+//     // if (head.classList.contains("active")) {
+//     //   head.classList.remove("active");
+//     // }
 
-    if (head.classList.contains("active")) {
-      accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
-      image.src = "./assets/images/icon-minus.svg";
-    } else {
-      accordionBody.style.maxHeight = 0;
-      image.src = "./assets/images/icon-plus.svg";
-    }
-  });
+//     if (head.classList.contains("active")) {
+//       accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
+//       image.src = "./assets/images/icon-minus.svg";
+//     } else {
+//       accordionBody.style.maxHeight = 0;
+//       image.src = "./assets/images/icon-plus.svg";
+//     }
+//   });
+// });
+
+// accordionHead.forEach((head) => {
+//   head.addEventListener("click", () => {
+//     let accordionBody = head.nextElementSibling;
+//     accordionBody.classList.toggle("active");
+
+//     let image = head.firstElementChild;
+//     // console.log(image);
+//     // if (head.classList.contains("active")) {
+//     //   head.classList.remove("active");
+//     // }
+
+//     if (accordionBody.classList.contains("active")) {
+//       // accordionBody.style.maxHeight = "fit-content";
+//       accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
+//       image.src = "./assets/images/icon-minus.svg";
+//     } else {
+//       accordionBody.style.maxHeight = 0;
+//       image.src = "./assets/images/icon-plus.svg";
+//     }
+//   });
+// });
+
+const accordionSection = document.querySelector(".accordion");
+const accordions = accordionSection?.querySelectorAll(".accordion--box");
+
+accordions?.forEach((accordion) => {
+  const accordionHead = accordion.querySelector(".accordion__head");
+  const handleClick = (element) => {
+    console.log(element);
+    accordions.forEach((_accordion) => {
+      const accordionContent = _accordion.querySelector(".accordion__content");
+      accordionContent?.classList.remove("active");
+    });
+    const parentContent = element.parentElement?.querySelector(
+      ".accordion__content"
+    );
+    if (parentContent) parentContent.classList.toggle("active");
+  };
+  accordionHead?.addEventListener("click", () => handleClick(accordionHead));
 });
